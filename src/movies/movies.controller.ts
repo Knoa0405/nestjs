a228fdb802c6f3f3b8c09c9ee2299d5df0eee342
Knoa <mike1124@naver.com>
2021-04-12 02:05:59 +0900

    ✨ Initialize Settings

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Patch,
+  Body,
+  Query,
+} from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
+
+  @Get('search')
+  search(@Query('year') movieYear: string) {
+    return 'search movie from query' + movieYear;
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') movieId: string): Movie {
+    return this.moviesService.getOne(movieId);
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    return this.moviesService.create(movieData);
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: string) {
+    return this.moviesService.deleteOne(movieId);
+  }
+
+  @Patch('/:id')
+  update(@Param('id') movieId: string, @Body() updateData) {
+    console.log(updateData);
+    return {
+      updateMovie: movieId,
+      ...updateData,
+    };
+  }
+}
